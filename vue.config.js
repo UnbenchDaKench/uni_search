606bf13234df89df5823931e0130e737de752459
kingg_sleyy <nonnystar53@gmail.com>
2019-10-26 13:21:55 -0300

    changed the port

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000/',
+        target: 'https://unisearchapi-demo.herokuapp.com',
         changeOrigin: false,
         pathRewrite: {
           '^/api': ''
